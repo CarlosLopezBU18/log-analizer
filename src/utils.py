4695e170e8e13ee1de05806021df31ba269423ce
Carlos David López Hinojosa <backupcarlos18@gmail.com>
2025-01-30 20:15:48 +0100

    Reorganización

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,16 @@
 from enum import Enum
+from typing import List
+
+import json
 
 class Level(Enum):
     ERROR = 0
     INFO = 1
     WARNING = 2
 
+def load_json_files(json_files: List[str]):
+    dicts = []
+    for json_file in json_files:
+        dicts.append(json.load(json_file))
+
+    return dicts
--- test/test.py
+++ test/test.py
@@ -1,13 +1,27 @@
 # from src.parser import DataParser
 from os import listdir, mkdir, rmdir
 from os.path import exists
+from typing import List
 
-INPUT_PATH: str = '/Users/more/Desktop/code/DailyPython/log-analizer/logs'
+from src.parser import DataParser
+from src.utils import load_json_files
+
+LOGS_PATH: str = '/Users/more/Desktop/code/DailyPython/log-analizer/logs'
+JSON_TEST_PATH: str = '/Users/more/Desktop/code/DailyPython/log-analizer/test/json'
 OUTPUT_PATH: str = '/Users/more/Desktop/code/DailyPython/log-analizer/test/out'
 
-print(listdir(INPUT_PATH))
+TEST_FILES: List[str] = listdir(JSON_TEST_PATH)
+INPUT_FILES: List[str] = listdir(LOGS_PATH)
 
 if exists(OUTPUT_PATH):
     rmdir(OUTPUT_PATH)
 else: 
     mkdir(OUTPUT_PATH)
+
+dp = DataParser(OUTPUT_PATH)
+dp.load_sources(INPUT_FILES)
+
+expected_results = load_json_files(TEST_FILES)
+my_results = load_json_files(OUTPUT_PATH)
+
+assert expected_results == my_results, 'Han pasado todos los test'
